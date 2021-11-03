9a33cd5c9d48f7e353337b09e48c78d81d3df909
stringja <89584562+stringjaUC@users.noreply.github.com>
2021-11-03 00:52:32 +0000

    Got rid of unnecessary comments

--- Main.java
+++ Main.java
@@ -4,16 +4,6 @@ import javax.swing.SwingConstants;
 
 class Main {
   public static void main(String[] args) {
-    /*JFrame frame = new JFrame("Hello world");
-    frame.setSize(300, 300);
-    frame.setLocation(5, 5);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    JLabel label = new JLabel("Hello world", SwingConstants.CENTER);
-    frame.add(label);
-
-    frame.show();
-    */
     new Counter();
   }
 }
